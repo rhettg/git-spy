eda7bfe58b90943112fd172eeb1d061f2cdad8e7
Rhett Garber <rhettg@github.com>
2017-09-11 10:20:35 -0700

    officially a proxy

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"strings"
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 )
 
 func passwordCallback(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
@@ -19,22 +23,104 @@ func publicKeyCallback(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissio
 	return nil, nil
 }
 
+func sshAgentAuth() ssh.AuthMethod {
+	sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		log.Fatalf("Failed to open ssh agent")
+	}
+
+	return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+}
+
+func proxyUploadPack(c ssh.Channel, cmd string) (err error) {
+	config := &ssh.ClientConfig{
+		User:            "git",
+		Auth:            []ssh.AuthMethod{sshAgentAuth()},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", "github.com:22", config)
+	if err != nil {
+		return fmt.Errorf("Failed to connect: %v", err)
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		return fmt.Errorf("Failed to create new session: %v", err)
+	}
+
+	defer session.Close()
+
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to open stdout: %v", err)
+	}
+
+	stderr, err := session.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("Failed to open stderr: %v", err)
+	}
+
+	err = session.Start(cmd)
+	if err != nil {
+		return fmt.Errorf("Failed to start command: %v", err)
+	}
+
+	go func() {
+		for {
+			br := bufio.NewReader(stderr)
+			line, _, err := br.ReadLine()
+
+			if err != nil {
+				log.Printf("Failed to read: %v", err)
+				break
+			}
+
+			log.Printf("Error %s", line)
+		}
+		return
+	}()
+
+	go func() {
+		for {
+			br := bufio.NewReader(stdout)
+			buf, err := br.Peek(4)
+
+			if err != nil {
+				log.Printf("Failed to read: %v", err)
+				break
+			}
+
+			log.Printf("Stdout %v", buf)
+
+			break
+		}
+		return
+	}()
+
+	err = session.Wait()
+	if err != nil {
+		return fmt.Errorf("Command failed: %v", err)
+	}
+
+	return nil
+}
+
 func handleChannel(c ssh.Channel, r <-chan *ssh.Request) {
 	for req := range r {
 		log.Printf("Channel request: %s", req.Type)
 
 		if req.Type == "exec" {
-			// Parse out our payload (3 null bytes and a ENQ)
+			// Parse out our payload, stripping 3 null bytes and an ENQ
 			p := string(req.Payload[4:])
 			if strings.HasPrefix(p, "git-upload-pack") {
-				//proxyUploadPack(c)
-
 				req.Reply(true, nil)
 
-				_, err := c.Write([]byte("hello world\n"))
+				err := proxyUploadPack(c, p)
 				if err != nil {
-					log.Fatalf("Failed to write to channel")
+					log.Fatalf("Failed to write to channel: %v", err)
 				}
+
 				log.Printf("Wrote reply to channel")
 			} else {
 				log.Printf("Unknown exec '%s' command, failing", p)
