7dbb4540d3fe4e98fdb727cd3f25d30f35ecc60a
Rhett Garber <rhettg@github.com>
2017-09-15 10:14:55 -0700

    spy on server

--- gitspy/spy.go
+++ gitspy/spy.go
@@ -34,12 +34,47 @@ func (gs *GitSpy) ServerPipe() io.WriteCloser {
 	r, w := io.Pipe()
 
 	go func() {
-		_, err := io.Copy(gs.c, r)
-		if err != nil && err != io.EOF {
-			r.CloseWithError(fmt.Errorf("Failed writing to client: %v", err))
-			return
+		b := make([]byte, 65516)
+		for {
+			n, err := ParsePktLine(r, b)
+			if err != nil && err != ErrFlushPkt {
+				log.Printf("Error parsing from server: %v", err)
+				if err == io.EOF {
+					log.Printf("EOF from server")
+				} else {
+					r.CloseWithError(fmt.Errorf("Failed parsing from server: %v", err))
+				}
+
+				break
+			}
+
+			if err == ErrFlushPkt {
+				log.Printf("S: FLUSH")
+				_, err = WritePktLineFlush(gs.c)
+				if err != nil {
+					r.CloseWithError(fmt.Errorf("Failed writing to client: %v", err))
+					break
+				}
+				// Now that we've flushed, just send the rest direct
+				_, err := io.Copy(gs.c, r)
+				if err != nil {
+					r.CloseWithError(fmt.Errorf("Failed writing to client: %v", err))
+				}
+				break
+
+			} else {
+				log.Printf("S: %s", b[0:n])
+
+				_, err = WritePktLine(gs.c, b[0:n])
+				if err != nil {
+					log.Printf("EOF writing to client")
+					r.CloseWithError(fmt.Errorf("Failed writing to client: %v", err))
+					break
+				}
+			}
 		}
 
+		//_, err := io.Copy(gs.c, r)
 		log.Printf("ServerPipe exited, closing client")
 		gs.c.Close()
 
